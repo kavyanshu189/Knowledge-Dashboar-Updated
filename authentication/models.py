189e5f6b4233fe61d9e38d1f70e001fb273577bc
Akshay Srivastava <68045904+Akshay-Srivastava@users.noreply.github.com>
2022-02-23 16:58:27 +0530

    front end changes
    close button working

--- authentication/models.py
+++ authentication/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
     
 class Login(models.Model):
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=10)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=20)
 
